f7bf651e1d5ce2a4a06cdf5b58c41c88259f6932
pavit <pavit@Pavithra.mynet>
2023-09-06 16:55:59 +0100

    Committing Add Java Project

---
+++ MyFirstJava/Add.java
@@ -0,0 +1,12 @@
+class Add
+{
+ public static void main(String a[])
+ {
+   int num1 = 6;
+   int num2 = 5;
+   int num3 = num1+num2;
+   System.out.println(num1);
+   System.out.println(num2);
+   System.out.println(num3);
+   }
+}
