13f36315b7789b293ece8243bba85535ecc4c916
pavit <pavit@Pavithra.mynet>
2023-09-08 18:27:10 +0100

    Commit Char Program

---
+++ MyFirstJava/Char.java
@@ -0,0 +1,10 @@
+class Char
+{
+ public static void main(String a[])
+ {
+    char c = '}';
+    System.out.println((int)c); // ASCII value of given value
+
+    System.out.print((char)125); // ASCII to actual char which is reverse
+   }
+}
